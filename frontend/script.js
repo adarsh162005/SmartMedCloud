380cb86c5f0cb8bad2c769803a812146f7c3645a
Adarsh V Kapse <adarshvkapse@gmail.com>
2026-05-11 19:41:54 +0530

    Connected frontend to deployed backend

--- frontend/script.js
+++ frontend/script.js
@@ -1,7 +1,7 @@
 // ================= API BASE URL =================
 
 const API_BASE =
-    "http://127.0.0.1:5000";
+    "https://smartmed-backend-h7qx.onrender.com";
 
 // ================= THEME =================
 
